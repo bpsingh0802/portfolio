cdcbeffc421021685a00c0e46ba7aaf8f98b344d
bpsingh0802 <80077777+bpsingh0802@users.noreply.github.com>
2025-03-11 11:10:53 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll);
+
+window.addEventListener("load", revealElementOnScroll);
+
+
+// ==================== card text=============
+function toggleText(element) {
+  var parent = element.parentElement;
+  parent.classList.toggle('expanded');
+
+  var extraText = parent.querySelector('.extra-text');
+  var moreText = parent.querySelector('.more-text');
+
+  if (parent.classList.contains('expanded')) {
+    extraText.style.display = "inline";
+    moreText.style.display = "none";
+  } else {
+    extraText.style.display = "none";
+    moreText.style.display = "inline";
+  }
+}
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+  var dataDiv = document.getElementById('data');
+  if (dataDiv.classList.contains('hidden')) {
+      dataDiv.classList.remove('hidden');
+      this.textContent = 'Hide Data';
+  } else {
+      dataDiv.classList.add('hidden');
+      this.textContent = 'Show Data';
+  }
+});
